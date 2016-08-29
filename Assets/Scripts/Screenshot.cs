91d4a068416a07f821cc6e265578913c8d1680ef
Thomas Tkatchenko <thomas@thank-etc-ok.com>
2016-08-29 10:09:10 +1000

    Add screenshot script and shot

---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour {
+
+  // Update is called once per frame
+  void Update() {
+    if (Input.GetKey(KeyCode.Space)) {
+      Application.CaptureScreenshot(System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png");
+    }
+  }
+}

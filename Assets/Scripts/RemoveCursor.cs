f463e076eda12a7c504e140f00c0f94c9894fe6e
Thomas Tkatchenko <thomas@thank-etc-ok.com>
2016-09-03 10:56:45 +1000

    Remove cursor

---
+++ Assets/Scripts/RemoveCursor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveCursor : MonoBehaviour {
+
+  void Start() {
+    Cursor.visible = false;	
+  }
+}
